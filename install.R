a8c619426b29dc357e0f1bb3c90b8d1e0a90a02c
dbbaskette <dbbaskette@gmail.com>
2016-08-22 14:55:39 -0400

    PivotalR

--- install.R
+++ install.R
@@ -1,2 +1,10 @@
+## Default repo
+local({r <- getOption("repos")
+       r["CRAN"] <- "http://cran.r-project.org" 
+       options(repos=r)
+})
 install.packages('evaluate', repos='http://cran.us.r-project.org')
 install.packages('knitr', repos='http://cran.us.r-project.org')
+install.packages('PivotalR', repos='http://cran.us.r-project.org')
+library(PivotalR)
+
